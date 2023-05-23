29208f4e3f3a2c7e72ab71485e8666f597e8f3aa
parnianRaf <parnian.rafie58@gmail.com>
2023-05-23 04:14:35 +0330

    first
    4Repositorie(Product-Auction-Offer-DirectOrder)

---
+++ AppCore/DtoModels/DirectOrder/EditDirectOrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AppCore.DtoModels.Product
+{
+	public class EditAuctionDto
+	{
+        public int Id { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public int SellerId { get; set; }
+    }
+}
+
